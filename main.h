75f3601fda9c79c6789db66eae47a7f28553bf7c
Amin <alarrouchi@gmail.com>
2023-07-22 13:05:54 -0700

    creation of main.h

---
+++ main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
