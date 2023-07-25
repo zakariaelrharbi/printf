cd0b51c82da149bf53ed8304fbb6d0889642889a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-07-25 01:28:36 +0100

    update print decimal

--- print_decimal.c
+++ print_decimal.c
@@ -1,40 +1,46 @@
 #include "main.h"
 
 /**
- * print_decimal - Prints a decimal integer and returns the number of characters printed.
+ * print_decimal - Prints a decimal integer.
  * @args: The va_list containing the decimal integer argument.
  * Return: The number of characters printed.
  */
 
-int print_decimal(va_list args) {
-    int num = va_arg(args, int);
-    int count = 0;
-    int div = 1;
+int print_decimal(va_list args)
+{
+	int num = va_arg(args, int);
+	int count = 0;
+	int div = 1;
+	int temp = num;
+	int digit;
 
-    if (num == 0) {
-        _putchar('0');
-        return 1;
-    }
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-    if (num < 0) {
-        _putchar('-');
-        count++;
-        num = -num;
-    }
+	if (num < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -num;
+	}
 
-    int temp = num;
-    while (temp != 0) {
-        temp /= 10;
-        div *= 10;
-    }
+	while (temp != 0)
+	{
+		temp /= 10;
+		div *= 10;
+	}
 
-    while (div > 1) {
-        div /= 10;
-        int digit = num / div;
-        _putchar(digit + '0');
-        count++;
-        num %= div;
-    }
+	while (div > 1)
+	{
+		div /= 10;
+		digit = num / div;
+		_putchar(digit + '0');
+		count++;
+		num %= div;
+	}
 
-    return (count);
+	return (count);
 }
