4ea2be04f57cbfb07c90dcb8fd70ff09e5f231b6
Amin <alarrouchi@gmail.com>
2023-07-25 16:04:09 -0700

    update

--- print_int.c
+++ print_int.c
@@ -8,39 +8,39 @@
 
 int print_decimal(va_list args)
 {
-    int num = va_arg(args, int);
-    int count = 0;
-    int div = 1;
-    int temp = num;
-    int digit;
+	int num = va_arg(args, int);
+	int count = 0;
+	int div = 1;
+	int temp = num;
+	int digit;
 
-    if (num == 0)
-    {
-        _putchar('0');
-        return (1);
-    }
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-    if (num < 0)
-    {
-        _putchar('-');
-        count++;
-        num = -num;
-    }
+	if (num < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -num;
+	}
 
-    while (temp != 0)
-    {
-        temp /= 10;
-        div *= 10;
-    }
+	while (temp != 0)
+	{
+		temp /= 10;
+		div *= 10;
+	}
 
-    while (div > 1)
-    {
-        div /= 10;
-        digit = num / div;
-        _putchar(digit + '0');
-        count++;
-        num %= div;
-    }
+	while (div > 1)
+	{
+		div /= 10;
+		digit = num / div;
+		_putchar(digit + '0');
+		count++;
+		num %= div;
+	}
 
-    return (count);
+	return (count);
 }
