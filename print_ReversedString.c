9cf80e3ac07f28769f439c19e57a7c8d3236727c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-07-25 01:31:13 +0100

    update print reversed string

--- print_ReversedString.c
+++ print_ReversedString.c
@@ -4,15 +4,18 @@
  * printReversedString - Prints the characters of a string in reverse order.
  * @args: A va_list containing the variable argument list.
  *
- * Return : none
+ * Return: none
  */
-void printReversedString(va_list args)
+int printReversedString(va_list args)
 {
-	const char *str = va_arg(args, const char*);
-	int len = _strlen(str);
+	char *str = va_arg(args, char*);
+	int i, j = 0;
 
-	for (int i = len - 1; i >= 0; i--)
-	{
-		_putchar(str[i]);
-	}
+if (str == NULL)
+	str = "(null)";
+while (str[j] != '\0')
+	j++;
+for (i = j - 1; j >= 0; i--)
+	_putchar(str[i]);
+return (j);
 }
