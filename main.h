018d426e7c30e0671db5505eaa1a8bca024a5a30
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-07-25 01:22:08 +0100

    update main.h

--- main.h
+++ main.h
@@ -1,17 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdarg.h>
 #include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
-typedef struct {
-	char specifier;
-	int (*printer)(va_list);
-} Specifier;
+/**
+ * struct Specifier_s - is a custom data type defined.
+ * @sp: A pointer to a character.
+ * @p: A function pointer that points to a function returning an integer.
+*/
+typedef struct Specifier_s
+{
+char *sp;
+int (*p)();
+}
+Specifier_t;
 
 int _printf(const char *format, ...);
 int valid_precentage(const char *format, int *i);
@@ -20,18 +27,11 @@ int print_percent(void);
 int print_unsigned_integer(unsigned int n);
 int print_unsigned_octal(unsigned int n);
 int print_string(va_list args);
-int _strlen(va_list args);
-void printReversedString(va_list args);
+int _strlen(char *s);
+int printReversedString(va_list args);
 int print_char(va_list args);
 int print_integer(va_list args);
 int print_decimal(va_list args);
-
-
-
-
-
-
-
-
+int _puts(char *s);
 
 #endif
