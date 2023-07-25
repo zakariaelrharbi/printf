c43be579a21a7a2edcf0c8de1e149a22a1aa1e4c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-07-25 01:08:57 +0100

    update putchar

--- _putchar.c
+++ _putchar.c
@@ -8,5 +8,21 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
+}
+/**
+ * _puts - print string.
+ * @s: string.
+ * Return: the len of the str.
+ */
+int _puts(char *s)
+{
+int i = 0;
+
+while (s[i])
+{
+_putchar(s[i]);
+i++;
+}
+return (i);
 }
