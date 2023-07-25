f7bf39296ba93790eeacc8f7306bca34ff6d0057
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-07-25 01:26:22 +0100

    update print char

--- print_char.c
+++ print_char.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * printf_char - prints a char.
- * @val: arguments.
- * Return: 1.
- */
+ * print_char -  function used to print a single character from the variable
+* argument list (va_list).
+ * @args: The variable argument list.
+ * Return: The number of characters printed.
+*/
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+
 	_putchar(c);
 	return (1);
 }
