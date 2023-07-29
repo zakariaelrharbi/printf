8b0a87c00d6988de5042ad1b11b05afb4f7cd61b
Amin <alarrouchi@gmail.com>
2023-07-28 19:10:07 -0700

    trying to solve reversed string probléme

--- print_ReversedString.c
+++ print_ReversedString.c
@@ -4,18 +4,22 @@
  * printReversedString - Prints the characters of a string in reverse order.
  * @args: A va_list containing the variable argument list.
  *
- * Return: none
+ * Return: The number of characters printed.
  */
 int printReversedString(va_list args)
 {
 	char *str = va_arg(args, char*);
 	int i, j = 0;
 
-if (str == NULL)
-	str = "(null)";
-while (str[j] != '\0')
-	j++;
-for (i = j - 1; j >= 0; i--)
-	_putchar(str[i]);
-return (j);
+	if (str == NULL)
+	{
+		_puts("(null)");
+		return (6);
+	}
+
+	j = _strlen(str);
+	for (i = j - 1; i >= 0; i--)
+		_putchar(str[i]);
+
+	return (j);
 }
